89d479bf95aac5ff6b3de5b8b77d7d1feb4ebeb9
JCZ92 <jczhu28@gmail.com>
2024-03-12 20:32:26 -0400

    done

--- models/book.js
+++ models/book.js
@@ -1,9 +1,18 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
-
+// schema is to define the document
+// a model in Mongoose is a class with which we construct documents.
 var BookSchema = new Schema(
-  {}
+  {
+    title : {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String},
+    ISBN: {type: String},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}], // [] means enabling multipliicity
+    bookInstance: [{type: Schema.Types.ObjectId, ref: 'BookInstance'}],
+    url: {type: String}
+  }
 );
 
 //Export model
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,19 @@
 let BookInstance = require('../models/bookinstance');
+const getStatus = async () => {
+  try {
+    let status = await BookInstance.find({status: 'Available'}).select('book status').populate('book');
+    return status.map((s) => s.book.title + ": " + s.status);
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+exports.show_all_books_status = async function(res) {
+  try {
+    const statuses = await getStatus();
+    return res.send(statuses);
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
 }
